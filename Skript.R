5d738a2947dadc3ff5a43486c6562679712a51e2
Peter Okruhlica <peterokruhlica@gmail.com>
2024-11-20 12:46:28 +0100

    Kleiner Edit

--- Skript.R
+++ Skript.R
@@ -1,17 +1,15 @@
-setwd("C:/Users/peter/Desktop/THD/3 Semester/Assistenzsysteme")
+setwd("C:/Users/peter/Desktop/THD/3 Semester/Assistenzsysteme/Projekt_Bauer")
 
 data <- read.csv("apartments_rent_pl_2024_06.csv", header=TRUE, sep=",", fill=TRUE, stringsAsFactors = TRUE)
-data2 <- read.csv("apartments_rent_pl_2024_06_bereinigt.csv", header=TRUE, sep=";", fill=TRUE, stringsAsFactors = TRUE)
+data <- read.csv("apartments_rent_pl_2024_06_bereinigt.csv", header=TRUE, sep=",", fill=TRUE, stringsAsFactors = TRUE)
 
 View(data)
-View(data2)
 
 remove(data)
-remove(data2)
 
-na_counts_base <- colSums(is.na(data2))
+NA_count <- colSums(is.na(data))
 print(na_counts_base)
 
+data$hasElevator[data2$hasElevator == ""] <- "no"
 
-
-remove(duplicates_count)
+remove(...)
